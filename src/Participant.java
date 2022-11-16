fecc462865c1245cbc75ae8f9d3e238606697d7c
Anastasiya Prudnikova <Anasatasiya Prudnikova>
2022-11-16 16:40:59 -0600

    made changes

---
+++ src/Participant.java
@@ -0,0 +1,66 @@
+
+    public class Participant {
+
+         /*
+        Declare static instance variables to store below information
+        total number of participants -> variable name can be totalNumberOfParticipants
+        total number of male participants -> variable name can be totalNumberOfMaleParticipants
+        total number of female participants -> variable name can be totalNumberOfFemaleParticipants
+         */
+
+        // 2. Instance variables
+        // Static -> belong to the Class. What does every sword has?
+        public static int totalNumberOfParticipants;
+        public static int totalNumberOfMaleParticipants;
+        public static int totalNumberOfFemaleParticipants;
+
+        /*
+        Declare instance variables for each participant as below
+        name -> can be String
+        age -> can be int
+        gender -> can be char
+         */
+
+        // Noo-static -> belong to the Object. What changing sword by sword?
+        public String name;
+        public int age;
+        public char gender;
+
+        public Participant(String name, int age, char gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+//Create a static void method addParticipants() that will increase totalNumberOfParticipants by 1
+
+
+        //Create a static void method addMaleParticipants() that will increase totalNumberOfMaleParticipants by 1
+
+
+        //Create a static void method addFemaleParticipants() that will increase totalNumberOfFemaleParticipants by 1
+
+
+
+        //Override toString() method and implement a new body that will return all information about a Participant
+
+        public static void addParticipants(){
+            totalNumberOfParticipants++;
+        }
+        public static void addFemaleParticipants(){
+            totalNumberOfFemaleParticipants++;
+        }
+        public static void addMaleParticipants(){
+            totalNumberOfMaleParticipants++;
+        }
+
+        @Override
+        public String toString() {
+            return "Participant{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+
---
+++ src/SurveyQuestions.java
@@ -0,0 +1,20 @@
+public class SurveyQuestions {
+
+    /*
+    Declare static final instance variables for interview questions below
+    Would you like to join our survey? (Y/N) -> variable name can be askToJoin
+    What is your name? -> variable name can be askName
+    How old are you? -> variable name can be askAge
+    What is your gender? (F/M) -> -> variable name can be askGender
+     */
+
+    //  Instance variables
+    // Static -> belong to the Class. What does every sword has?
+    //public static final boolean hasHandle = true;
+
+    public static final String askToJoin = "Would you like to join our survey? (Y/N)";
+    public static final String askName = "What is your name?";
+    public static final String askAge = "How old are you?";
+    public static final String askGender = "What is your gender? (F/M)";
+
+}
